ba3ae79c5bc355ea8433111f05050ce7f60a31c8
Mathieu Post <mathieupost@gmail.com>
2025-06-26 13:15:16 +0200

    feat: add RefProps type helper

---
+++ src/core/props.ts
@@ -0,0 +1,15 @@
+import { SubscriptionRef } from "effect";
+
+export type RefProps<T extends object> = {
+  [K in keyof T]: T[K] extends SubscriptionRef.SubscriptionRef<infer U>
+    ? T[K]
+    : T[K] extends undefined // Handle optional properties
+    ? T[K] extends infer R // Distribute over union types for R
+      ? R | SubscriptionRef.SubscriptionRef<R> | undefined
+      : never
+    : T[K] extends infer U
+    ? U extends infer R
+      ? R | SubscriptionRef.SubscriptionRef<R>
+      : never
+    : never;
+};
--- src/frontend.tsx
+++ src/frontend.tsx
@@ -1,16 +1,38 @@
 /** @jsxImportSource ./core */
 import { Effect, SubscriptionRef } from "effect";
+import { RefProps } from "./core/props";
 import { mount } from "./core/renderer";
 
-const Counter = () =>
+const Counter = ({ label = "Counter" }: RefProps<{ label?: string }>) =>
   Effect.gen(function* (_) {
     const count = yield* _(SubscriptionRef.make(0));
     const increment = SubscriptionRef.update(count, (n) => n + 1);
 
     return (
       <div>
-        <p>Count: {count}</p>
+        <p>
+          {label}: {count}
+        </p>
         <button onClick={increment}>Increment</button>
+      </div>
+    );
+  });
+
+const LabeledCounter = () =>
+  Effect.gen(function* (_) {
+    const label = yield* _(SubscriptionRef.make(""));
+    const setLabel = (l: string) => SubscriptionRef.update(label, () => l);
+    return (
+      <div>
+        <input
+          type="text"
+          value={label}
+          onInput={(e: Event) => setLabel((e.target as HTMLInputElement).value)}
+          onChange={() => {
+            console.log("onChange");
+          }}
+        />
+        <Counter label={label} />
       </div>
     );
   });
@@ -18,7 +40,7 @@ const Counter = () =>
 const App = () => (
   <div>
     <Counter />
-    <Counter />
+    <LabeledCounter />
   </div>
 );
 
