fb29cd0e8ec00c605f895908f0fddeb1d5019c71
Mathieu Post <mathieupost@gmail.com>
2025-06-27 18:19:32 +0200

    feat(lexer): scan single char tokens

--- packages/effect-ui/src/compiler/lexer.ts
+++ packages/effect-ui/src/compiler/lexer.ts
@@ -38,13 +38,78 @@ export class Lexer {
     // We'll implement this in the next steps.
     const char = this.advance();
 
-    // For now, let's just log the character and move on.
-    console.log(`Scanning character: ${char}`);
+    switch (char) {
+      case "<":
+        this.addToken(TokenType.LessThan);
+        break;
+      case ">":
+        this.addToken(TokenType.GreaterThan);
+        break;
+
+      case "/":
+        this.addToken(TokenType.Slash);
+        break;
+      case "=":
+        this.addToken(TokenType.Equals);
+        break;
+      case "{":
+        this.addToken(TokenType.OpenBrace);
+        break;
+      case "}":
+        this.addToken(TokenType.CloseBrace);
+        break;
+      case ".":
+        if (this.match(".") && this.match(".")) {
+          this.addToken(TokenType.Spread);
+        } else {
+          this.addToken(TokenType.Dot);
+        }
+        break;
+
+      // Ignore whitespace
+      case " ":
+      case "\r":
+      case "\t":
+        break;
+
+      case "\n":
+        this.line++;
+        this.col = 1;
+        break;
+
+      default:
+        // For now, we'll just log an error for unrecognized characters.
+        // We will handle text nodes, identifiers, and strings later.
+        console.error(
+          `[Line ${this.line}] Error: Unexpected character: ${char}`
+        );
+        break;
+    }
   }
 
   private advance(): string {
     this.current++;
     this.col++;
     return this.source.charAt(this.current - 1);
+  }
+
+  private match(expected: string): boolean {
+    if (this.isAtEnd()) return false;
+    if (this.source.charAt(this.current) !== expected) return false;
+
+    this.current++;
+    this.col++;
+    return true;
+  }
+
+  private addToken(type: TokenType, literal: any = null): void {
+    const text = this.source.substring(this.start, this.current);
+    this.tokens.push({
+      type,
+      lexeme: text,
+      literal,
+      line: this.line,
+      col: this.col - text.length + 1, // beginning of the token
+    });
   }
 }
